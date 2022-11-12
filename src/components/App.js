4a6d01582cd6fbecbbe168599a86691576af3ad8
Chase Lukasiewicz <chaseluka@gmail.com>
2022-11-11 17:50:01 -0700

    banner

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "../style/App.css";
 import Home from "./Home";
 import Auth from "./Auth";
+import Banner from "./Banner";
 
 function App() {
   const [displayPopup, setDisplayPopup] = useState(false);
@@ -11,7 +12,8 @@ function App() {
   //allow click on login or sign up to show the authentication view.
 
   return (
-    <div>
+    <div className="app-container">
+      <Banner />
       <Home setLogin={setLogin} setDisplayPopup={setDisplayPopup} />
       <Auth
         displayPopup={displayPopup}
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
 import Feed from "./Feed";
 import Footer from "./Footer";
 import Sidebar from "./Sidebar";
-import Banner from "./Banner";
+
 const Home = ({ setLogin, setDisplayPopup }) => {
+  const [userIsSignedIn, setUserIsSignedIn] = useState(false);
+
+  const logOut = () => {
+    signOut(getAuth());
+  };
+
+  useEffect(() => {
+    onAuthStateChanged(getAuth(), (user) => {
+      if (user) {
+        setUserIsSignedIn(true);
+      } else {
+        setUserIsSignedIn(false);
+      }
+    });
+  });
+
   return (
     <main id="home">
-      <Banner />
       <Feed />
       <Sidebar />
-      <Footer setLogin={setLogin} setDisplayPopup={setDisplayPopup} />
+      {(() => {
+        if (!userIsSignedIn)
+          return (
+            <Footer setLogin={setLogin} setDisplayPopup={setDisplayPopup} />
+          );
+      })()}
+      <div onClick={logOut}>Log Out</div>
     </main>
   );
 };
